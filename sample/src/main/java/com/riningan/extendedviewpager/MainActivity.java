696c4418bb24b8e6f6a6c4f3eebdf7e41783f7e6
vahmarov <vahmarov@stream.ru>
2018-12-13 13:28:45 +0500

    version 1.1

--- sample/src/main/java/com/riningan/extendedviewpager/MainActivity.java
+++ sample/src/main/java/com/riningan/extendedviewpager/MainActivity.java
@@ -1,4 +1,4 @@
-package com.riningan.controlledviewpager;
+package com.riningan.extendedviewpager;
 
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.riningan.widget.ExtendedViewPager;
 
 
